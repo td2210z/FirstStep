2a0549f2e923387aab5edb9c30440d11976890e3
td2210z <devthang2210@gmail.com>
2024-06-12 23:34:05 +0700

    up

---
+++ FirstStep/src/Main.java
@@ -0,0 +1,35 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        /*int min_value = Integer.MIN_VALUE;
+        int max_value = Integer.MAX_VALUE;
+
+        System.out.println(min_value + " " + max_value);*/
+
+
+//        byte a = Byte.MAX_VALUE;
+//        byte b = Byte.MIN_VALUE;
+//        System.out.println(a + " " + b);
+
+
+//        short a= Short.MAX_VALUE;
+//        short b = Short.MIN_VALUE;
+//        System.out.println(a + " " + b);
+
+
+//        long a = Long.MAX_VALUE;
+//        long b = Long.MIN_VALUE;
+//        System.out.println(a + " " + b);
+
+
+//        double a = Double.MIN_VALUE;
+//        double b = Double.MAX_VALUE;
+//        System.out.println(a + " " + b);
+
+        char a = 'D';
+        System.out.println(a);
+    }
+}
